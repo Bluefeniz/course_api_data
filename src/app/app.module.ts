d68806ca03480d50e37fc5990a3c0e1cd43aebb2
Bluefeniz <flaviofrancardi@libero.it>
2023-11-06 14:40:07 +0100

    build fails. Test for importing Material package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { DataService } from './data.service';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -22,7 +23,8 @@ import { DataService } from './data.service';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule
 
   ],
   providers: [DataService],
